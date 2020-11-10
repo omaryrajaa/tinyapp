f9548d766ed5e34d77f2532772ba56d0cc4bf337
Rajaa Omary <omary.rajaa@gmail.com>
2020-11-10 01:03:10 +0000

    Add a Header Partial

--- express_server.js
+++ express_server.js
@@ -19,8 +19,8 @@ app.get("/urls", (req, res) => {
 
 });
 
-app.get("/urls:shortURL", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL.substring(1)]};
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 
 });
